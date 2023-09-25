bef5b26b9af5ad9c6c92ad4335855acaf6c929c4
Maicon Gilton <maicongilton@gmail.com>
2023-09-25 17:28:36 -0300

    feat: add portfolio tab screen

---
+++ src/screens/portfolio/index.tsx
@@ -0,0 +1,29 @@
+import CustomHeader from '@components/CustomHeader'
+import CustomText from '@components/CustomText'
+import { THEME_COLORS } from '@theme/colors'
+import React from 'react'
+import { StyleSheet } from 'react-native'
+import { ScrollView } from 'react-native-gesture-handler'
+
+const PortfolioScreen = () => {
+  return (
+    <>
+      <CustomHeader title='Portfolio' />
+
+      <ScrollView style={styles.container}>
+        <CustomText>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </CustomText>
+      </ScrollView>
+    </>
+  )
+}
+
+export default PortfolioScreen
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: THEME_COLORS.WHITE,
+    padding: 20
+  }
+})
