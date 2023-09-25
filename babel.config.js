fe54bb545256a5f49af23abf4e04765ac0564279
Maicon Gilton <maicongilton@gmail.com>
2023-09-25 19:53:33 -0300

    fix: missing close brackets

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  presets: [
-    ['module:metro-react-native-babel-preset'],
-  ],
+  presets: ['module:metro-react-native-babel-preset'],
   plugins: [
     [
       'babel-plugin-root-import',
@@ -44,6 +42,7 @@ module.exports = {
             rootPathSuffix: './src/mocks',
           },
         ],
-      },
-    ],
+      }
+    ]
   ]
+}
