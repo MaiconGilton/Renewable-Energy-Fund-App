3be4ef118e52b97697708bf56e3d54890b408d0c
Maicon Gilton <maicongilton@gmail.com>
2023-09-25 15:47:33 -0300

    feat: setup typescript and babel

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,49 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
-};
+  presets: [
+    ['module:metro-react-native-babel-preset'],
+  ],
+  plugins: [
+    [
+      'babel-plugin-root-import',
+      {
+        paths: [
+          {
+            rootPathPrefix: '@utils',
+            rootPathSuffix: './src/utils',
+          },
+          {
+            rootPathPrefix: '@services',
+            rootPathSuffix: './src/services',
+          },
+          {
+            rootPathPrefix: '@components',
+            rootPathSuffix: './src/components',
+          },
+          {
+            rootPathPrefix: '@assets',
+            rootPathSuffix: './src/assets',
+          },
+          {
+            rootPathPrefix: '@theme',
+            rootPathSuffix: './src/theme',
+          },
+          {
+            rootPathPrefix: '@screens',
+            rootPathSuffix: './src/screens',
+          },
+          {
+            rootPathPrefix: '@store',
+            rootPathSuffix: './src/store',
+          },
+          {
+            rootPathPrefix: '@navigation',
+            rootPathSuffix: './src/navigation',
+          },
+          {
+            rootPathPrefix: '@mocks',
+            rootPathSuffix: './src/mocks',
+          },
+        ],
+      },
+    ],
+  ]
