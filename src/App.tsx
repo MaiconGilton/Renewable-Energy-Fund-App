ce7868cee8c67829df774b8c17e4037b283c86ad
Maicon Gilton <maicongilton@gmail.com>
2023-09-25 15:59:30 -0300

    refactor: rearrange folder struture

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+
+    </SafeAreaView>
+  );
+}
+
+export default App;
